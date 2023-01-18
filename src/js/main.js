b103133c292e2ab1907801428c55d4b5c90a8e26
Yaroslav <yaroslav.web9@gmail.com>
2023-01-18 17:29:47 +0300

    added a timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
     form: 1,
     type: 'tree',
   };
+  const deadline = '2023-01-30';
 
   changeModalState(modaleState);
   modals();
@@ -18,4 +20,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modaleState);
+  timer('.timer1', deadline);
 });
